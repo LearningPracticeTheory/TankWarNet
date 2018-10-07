9dd00f4db408f4148ba1e4bed8b3756cead47675
TinyA <1097912818@qq.com>
2018-10-07 16:45:47 +0800

    new otherTanks & synchronized each Tanks on each Clients

--- src/TankNewMsg.java
+++ src/TankNewMsg.java
@@ -51,15 +51,43 @@ public class TankNewMsg implements Msg {
 			if(tc.myTank.ID == ID) {
 				return;
 			}
+			
 			int x = dis.readInt();
 			int y = dis.readInt();
 			Direction dir = Direction.values()[dis.readInt()]; //find index and getValuse of index
 			boolean good = dis.readBoolean();
 //System.out.println("ID:" + ID + " x:" + x + " y:" + y + " dir:" + dir + " good:" + good);
-			Tank t = new Tank(x, y, good, tc);
-			t.dir = dir;
-			t.ID = ID;
-			tc.tanks.add(t);
+			
+//------------------------------------------------------------------
+/*
+ * new otherTanks & synchronized each Tanks on each Clients
+ */
+			boolean exist = false;
+			for(int i = 0; i < tc.tanks.size(); i++) {
+				Tank t = tc.tanks.get(i);
+				if(t.ID == ID) {
+					exist = true;
+					break;
+				}
+			}
+			if(!exist) {
+				
+				TankNewMsg tnmMsg = new TankNewMsg(tc.myTank);
+				tc.nc.send(tnmMsg); //send tank_1 newMsg to Server, then Server send back to all the Clients
+				//1. tank_1 will receive the ownTanKNewMsg
+					//But break in first tank_1.ID == tank_1.ID; (myTank.ID == ID);
+				//2. tank_2 will receive the TankNewMsg too
+					//first compare tank_2.ID != tank_1.ID; (tank_2 are also have it's own myTank);
+					//keep going, loop all tanks, if this tank_1.ID exist break;
+					/*else if !exist send tank_2 newMsg to Server (like tank_1 send newMsg) & add new Tank*/
+				//2_1. tank_2 receive tank_2's TankNewMsg, but it's own TankNewMsg, so like step1, break;
+				//1_1. tank_1 receive tank_2's TankNewMsg, but on loop found it existed, so break too;
+				Tank t = new Tank(x, y, good, tc);
+				t.dir = dir;
+				t.ID = ID;
+				tc.tanks.add(t);
+			}
+//------------------------------------------------------------------
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
